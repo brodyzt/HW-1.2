57b815509410ed77854f1d2c3a96236264b109d7
brodyzt <brodyzt@gmail.com>
2015-06-25 15:12:55 -0400

    Initial commit

---
+++ Project.py
@@ -0,0 +1,64 @@
+def index (testlist, lookingFor):
+    i = 0
+    index = 0
+    try:
+        while i < len(testlist):
+            index = testlist.index(lookingFor,i)
+            i = index + 1
+            return index
+    except ValueError: #testlist.index() cannot find lookingFor
+        pass
+def sortList (list):
+    listSorted = False
+    while(listSorted == False):
+        listSorted = True
+        for x in range(0, len(list) - 1):
+            if(list[x] > list[x+1]):
+                temp = list[x+1]
+                tempAlph = alphabet[x+1]
+                list[x+1] = list[x]
+                alphabet[x+1] = alphabet[x]
+                list[x] = temp
+                alphabet[x] = tempAlph
+                listSorted = False
+def contentsOfFile(file):
+    tempString = ''
+    contents = file.readlines()
+    for line in contents:
+        tempString = str(tempString) + str(line)
+    return tempString
+
+
+
+print("Before continuing with the program, enter the plain English text into the 'Plain Text' file and enter the encrypted text into 'Plain Text'")
+while(raw_input("Type 'done' and hit enter once completed: ") != 'done'):
+    pass
+originalChar = []
+newChar = []
+for x in range(0, 2):
+    alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z', ' ']
+    if(x == 0):
+        text = contentsOfFile(open('Plain Text', 'r'))
+    else:
+        text = contentsOfFile(open('Encrypted Text', 'r'))
+    frequencies = []
+
+    for char in alphabet:
+        frequencies.append(text.count(char))
+    sortList(frequencies)
+    if(x == 0):
+        originalChar = alphabet
+    else:
+        newChar = alphabet
+    '''for x in range(0, len(alphabet)):
+        print(str(alphabet[x]) + ': ' + str(frequencies[x]))'''
+
+
+translatedText = ''
+textToDecrypt = contentsOfFile(open('Encrypted Text', 'r'))
+for char in textToDecrypt:
+    if(char in newChar):
+        indexNum = index(newChar, char)
+        translatedText = translatedText + originalChar[indexNum]
+
+print('\nDecrypted Text: ' + translatedText)
